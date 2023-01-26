3ec64a68ce78c86dc2d835678f739ef369b43453
vedavyas <vedavyas14042003@gmail.com>
2023-01-26 20:18:28 +0530

    made responsive

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const left_ul=document.getElementById("ul-left")
 const right_ul=document.getElementById("ul-right")
+const navele=document.querySelector(".nav-ele")
 const toggleButton =document.querySelector(".toggle__button")
 
 toggleButton.addEventListener("click",()=>{
   console.log("entered");
-  let vis= left_ul.getAttribute("data-expanded")
+  let vis= navele.getAttribute("data-expanded")
   console.log(vis);
   if (vis==="false") {
     left_ul.setAttribute("data-expanded","true")
     right_ul.setAttribute("data-expanded","true")
+    navele.setAttribute("data-expanded","true")
     toggleButton.setAttribute("aria-expanded","true")
   }
   else if(vis==="true"){
     left_ul.setAttribute("data-expanded","false")
     right_ul.setAttribute("data-expanded","false")
+    navele.setAttribute("data-expanded","false")
     toggleButton.setAttribute("aria-expanded","false")
   }
 })
